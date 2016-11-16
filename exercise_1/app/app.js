a74a3ce77625444808ecbee97ec349136472f457
Nicholas Johnson <nicholas@forwardadvance.com>
2016-11-16 09:07:21 +0000

    initial state

--- exercise_1/app/app.js
+++ exercise_1/app/app.js
@@ -1,22 +1,15 @@
-// Exercise - like counter
-
-// Extend the code so that we can count the number of likes. When the like counter emits a like event, have the counter in the parent template increase by one.
-// A user can like as many times as they want to.
 
 var AppComponent = ng.core
   .Component({
     selector: "app",
     template:
     `
-      <like (liked)="handleLike()">Hello</like>
-      {{message}}
+
     `
   })
   .Class({
     constructor: function() {
-      this.handleLike = function() {
-        this.message = "Thanks for liking us"
-      }
+
     }
   });
 
